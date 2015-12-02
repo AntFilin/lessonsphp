c01d0c683123c2b79f02d8b3d1a1f92bc75b7d63
antfilin <antonfilppov@gmail.com>
2015-12-02 21:14:54 +0300

    Папка post

--- post/post.html.php
+++ post/post.html.php
@@ -4,9 +4,18 @@
     <meta charset="utf-8" />
     <title>Публикации</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css" />
-
+    <link rel="stylesheet" href="../css/style.css" />
 </head>
 <body>
+    <nav class="navbar navbar-inverse">
+        <a class="blog-nav-item" href="#">Название сайта</a>
+        <a class="blog-nav-item" href="#">Публикации</a>
+        <a class="blog-nav-item" href="#">О проекте</a>
+    </nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
 <?php foreach($posts as $post) { ?>
     <div class="panel panel-default">
         <div class="panel-heading">
@@ -17,5 +26,23 @@
         </div>
     </div>
 <?php } ?>
+        </div>
+        <div class="col-md-4">
+            <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Сортировать по дате <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Самые новые</a></li>
+                    <li><a href="#">Самые старые</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/scripts.js"></script>
+
 </body>
 </html>
--- post/post.php
+++ post/post.php
@@ -1,9 +1,12 @@
 <?php
 
 $posts = array (
-     array ('title' => 'title of post1','body' => 'body of post1'),
-     array ('title' => 'title of post2','body' => 'body of post2')
-
+     array ('title' => 'title of post1','body' => 'body of post1','date' =>'2015-12-01 13:00:00'),
+     array ('title' => 'title of post2','body' => 'body of post2','date' =>'2015-12-02 13:00:00'),
+     
 );
+if($_GET["date_sort=old"]){
+
+}
 include 'post.html.php';
 
