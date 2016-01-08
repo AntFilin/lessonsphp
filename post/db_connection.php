c36e024974cae3e696dcfc0c2bebb22a73e374b3
antfilin <antonfilppov@gmail.com>
2016-01-08 14:58:16 +0300

    Папка post. Создал отдельный файл для подключения к БД и вывода информации.

--- post/db_connection.php
+++ post/db_connection.php
@@ -2,13 +2,28 @@
 $dblocation = "localhost";
 $dbuser = "root";
 $dbpasswd = "";
-$dbcnx = mysqli_connect($dblocation,$dbuser,$dbpasswd);
-if (!$dbcnx) {
+$dbname = "blog";
+
+$connection = mysqli_connect($dblocation,$dbuser,$dbpasswd, $dbname);
+
+if (!$connection) {
     exit ("<P> В настоящий момент сервер базы данных недоступен</P>");
 }
 else {
     echo('<P>Соединение установлено.</P>');
 }
 
+$query = "SELECT * FROM posts";
+$result = mysqli_query($connection, $query);
+if (!$result) {
+    die("Запрос не удался.");
+}
+while($row = mysqli_fetch_assoc($result)){
+    echo $row ["post_id"]. "<br />";
+    echo $row ["dt_post"]. "<br />";
 
+    echo "<hr />";
+}
+mysqli_free_result($result);
+mysqli_close($connection);
 
